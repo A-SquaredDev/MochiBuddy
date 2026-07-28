5db79194c06b1539eabcb12e076f196414acfbd9
aaronmckain <almckain@ksu.edu>
2026-07-28 00:52:10 -0500

    Swap the support contact to asquared.dev@gmail.com

--- MochiBuddy/You/MochiLinks.swift
+++ MochiBuddy/You/MochiLinks.swift
@@ -14,7 +14,7 @@ import UIKit
 enum MochiLinks {
     static let privacyPolicy = URL(string: "https://a-squareddev.github.io/MochiBuddy/privacy/")!
     static let termsOfUse = URL(string: "https://www.apple.com/legal/internet-services/itunes/dev/stdeula/")!
-    static let support = URL(string: "mailto:maaron41602@gmail.com")!
+    static let support = URL(string: "mailto:asquared.dev@gmail.com")!
     static let manageSubscriptions = URL(string: "https://apps.apple.com/account/subscriptions")!
     /// Deep link into the app's own page in Settings (notification toggles).
     static let systemSettings = URL(string: UIApplication.openSettingsURLString)!
